c260fc7362275b5c7e7885e0968888e98be3f1b5
Iwan-deer <59004704+Iwan-deer@users.noreply.github.com>
2019-12-20 21:23:25 +0800

    Create for循环练习.js

---
+++ for循环练习.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<script type="text/javascript">
+				//打印1-100所有奇数之和
+		/* 		var sum = 0;
+				for(n=1;n<=100 ;n++){
+					if (n%2 != 0){
+						sum += n;
+					}
+				}
+				document.write(sum + "\n")
+		 */
+		
+				//水仙花数:是一个三位数,每个位上的数的三次方之和等于他本身
+				var num = 100;
+				
+				for( ; num<1000 ; num++){
+					var a = parseInt(num%10);  //个位
+					var b = parseInt((num%100)/10);  //十位
+					var c = parseInt((num%1000)/100);  //百位
+					
+					 if (a*a*a+b*b*b+c*c*c==num){
+						console.log(num);
+					 }
+				}
+				
+			</script>
+	</body>
+</html>
